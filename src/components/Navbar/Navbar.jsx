9ffdd72000099596e7b4b70b58e28395e78875c2
rajvircodes <rajvirsinhgohil004@gmail.com>
2026-06-24 23:47:58 +0530

    working in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,19 @@
+import { Link } from "react-router-dom"
+import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>Navbar</h1>
-            <p>Navigate through</p>
-        </div>
+        <nav>
+            <div className="logo">
+                <h1>LOGO</h1>
+            </div>
+            <div className="links">
+                <Link to='/'>Home</Link>
+                <Link to='/about'>About</Link>
+                <Link to='/projects'>Projects</Link>
+                <Link to='/skills'>Skills</Link>
+            </div>
+        </nav>
     )
 }
 
