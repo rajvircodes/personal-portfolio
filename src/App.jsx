b6a40ee68c9676c8503e55452e8b092bea433c07
rajvircodes <rajvirsinhgohil004@gmail.com>
2026-08-15 23:32:05 +0530

    footer changed and smooth scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import AppRoutes from "./routes/AppRoutes";
 import Cursor from "./components/Cursor/Cursor";
+import SmoothScroll from "./components/SmoothScroll/SmoothScroll";
 
 function App() {
   return (
     <>
+      <SmoothScroll />
       <Cursor />
 
       <AppRoutes />
---
+++ src/components/SmoothScroll/SmoothScroll.jsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import Lenis from "lenis";
+
+const SmoothScroll = () => {
+    useEffect(() => {
+        const lenis = new Lenis({
+            duration: 1.2,
+            smoothWheel: true,
+            wheelMultiplier: 0.9,
+            touchMultiplier: 1.5,
+        });
+
+        let animationFrame;
+
+        const raf = (time) => {
+            lenis.raf(time);
+
+            animationFrame = requestAnimationFrame(raf);
+        };
+
+        animationFrame = requestAnimationFrame(raf);
+
+        return () => {
+            cancelAnimationFrame(animationFrame);
+            lenis.destroy();
+        };
+    }, []);
+
+    return null;
+};
+
+export default SmoothScroll;
